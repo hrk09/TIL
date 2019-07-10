0e9da4a55f6dc970b232a34163761b5a37a41451
hyeryeons2 <hyeryeons23@gmail.com>
2019-07-10 17:08:48 +0900

    Learn Jinja2

--- startCamp/03_day/flask/intro/app.py
+++ startCamp/03_day/flask/intro/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template 
 import datetime
 import random
 app = Flask(__name__)
 
 @app.route('/') # / 를 endpoint라고 함
 def hello():
-    return 'Hello Hailey!'
+    # return 'Hello Hailey!'
+    return render_template('index.html')
 
 
 @app.route('/ssafy')
@@ -40,12 +41,16 @@ def greeting_ziont():
 # Variable Routing
 @app.route('/greeting/<name>')
 def greeting(name):
-    return f'반갑습니다 {name} 님!'
+    # return f'반갑습니다 {name} 님!'
+    return render_template('greeting.html', html_name=name)
 
 @app.route('/cube/<int:num>')
 def cube(num):
     num ** 3
-    return f'{num}의 3 제곱은 {num ** 3} 입니다.'
+    # return f'{num}의 3 제곱은 {num ** 3} 입니다.'
+    return render_template('cube.html', html_num=num)
+    # result = num ** 3
+    # return render_template('cube.html', num=num, result=result)
 
 # 실습
 @app.route('/lunch/<int:people>')
@@ -53,6 +58,13 @@ def lunch(people):
     menu = ['김밥', '떡볶이', '라면']
     order = random.sample(menu, people)
     return str(order)
+
+
+@app.route('/movie')
+def movie():
+    movies = ['스파이더맨', '엔드게임', '기생충', '알라딘']
+    return render_template('movie.html', movies=movies)
+
 
 if  __name__ == '__main__':
     app.run(debug=True)
